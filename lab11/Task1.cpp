cd36783b7c09e9e28ecb8d206e9bd4ed4b78aa57
Saad Tauseef <126574907+DAAS69@users.noreply.github.com>
2025-05-01 16:16:11 +0500

    Task1.cpp

---
+++ lab11/Task1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class InvalidValueException : public exception {
+public:
+    const char* what() const noexcept override {
+        return "InvalidValueException - Age cannot be negative or exceed 120.";
+    }
+};
+
+void validateAge(int age) {
+    if (age < 0 || age > 120) {
+        throw InvalidValueException();
+    }
+}
+
+int main() {
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    try {
+        validateAge(age);
+        cout << "Valid age: " << age << endl;
+    } 
+    
+    catch (const InvalidValueException& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
